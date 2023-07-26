da6efa930a1cbdc43c471e38c9a63e2adc21980d
MarcoPetrin <petrinmarco@gmail.com>
2023-07-25 21:30:08 -0300

    Add novos arquivos

--- utils/create_contacts.py
+++ utils/create_contacts.py
@@ -35,10 +35,10 @@ if __name__ == '__main__':
     django_contacts = []
 
     for _ in range(NUMBER_OF_OBJECTS):
-        profile = fake.profile()        
+        profile = fake.profile()
+        email = profile['mail']
         first_name, last_name = profile['name'].split(' ', 1)
         phone = fake.phone_number()
-        email = profile['mail']
         created_date: datetime = fake.date_this_year()
         description = fake.text(max_nb_chars=100)
         category = choice(django_categories)
