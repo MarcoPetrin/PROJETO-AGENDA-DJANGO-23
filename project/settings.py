027bdaf5cb66118cf08db980b5e88b0d70265200
MarcoPetrin <petrinmarco@gmail.com>
2023-07-25 21:46:58 -0300

    Add novos arquivos

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-og^dro09a_wby@kcwexce)8iz%gs4sc+i6i@nfcehm3*a_+juw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
